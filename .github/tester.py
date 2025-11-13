75a8de5ecd51696c1fb24e671e490403d8258e17
wan06-user <mokatv25@gmail.com>
2025-11-14 03:10:26 +0900

    Create tester.py

---
+++ .github/tester.py
@@ -0,0 +1,12 @@
+import geo.utils as utils
+import math
+
+#calculate the length of hypotenuse(c) when a=3 and b=4
+a,b = 3, 4
+c = math.sqrt(a**2 + b**2)
+print('c= ',c)
+
+#calculate the area of circle with radius r = 10
+r = 10
+area = math.pi * r**2
+print('area= ',area)
